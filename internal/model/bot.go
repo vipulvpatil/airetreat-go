5c7d1d3cf7c3272502cd7d028717b9760ed44c5e
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-02-15 21:28:09 +0530

    Added random bot name generation

--- internal/model/bot.go
+++ internal/model/bot.go
@@ -1,21 +1,44 @@
 package model
 
 import (
+	"math/rand"
+
 	"github.com/pkg/errors"
 	"github.com/vipulvpatil/airetreat-go/internal/utilities"
 )
 
+const totalNumberOfBotsPerGame = 5
+
 type Bot struct {
-	id string
+	id   string
+	name string
 }
 
 type BotOptions struct {
-	Id string
+	Id   string
+	Name string
 }
 
 func NewBot(opts BotOptions) (*Bot, error) {
 	if utilities.IsBlank(opts.Id) {
 		return nil, errors.New("cannot create bot with an empty id")
 	}
-	return &Bot{id: opts.Id}, nil
+	if utilities.IsBlank(opts.Name) {
+		return nil, errors.New("cannot create bot with an empty name")
+	}
+	return &Bot{
+		id:   opts.Id,
+		name: opts.Name,
+	}, nil
+}
+
+func RandomBotNames(randomSeed int64) []string {
+	botNames := []string{
+		"C-21PO", "R4-D4", "Gart", "HAL 9999", "Avis", "ED-I", "T-5000", "Davide", "B.O.B.Z", "The Machy-ne", "GLaDOODLES", "JARV-EESE", "The Hivey-five", "T-3PO", "InfoData", "Sort", "Electronic Device-209", "T-800X", "RoboCupp", "EVE-a-L", "GLaDOSE",
+	}
+	rand.Seed(randomSeed)
+	rand.Shuffle(len(botNames), func(i, j int) {
+		botNames[i], botNames[j] = botNames[j], botNames[i]
+	})
+	return botNames[0:totalNumberOfBotsPerGame]
 }
--- internal/model/bot_test.go
+++ internal/model/bot_test.go
@@ -22,12 +22,21 @@ func Test_NewBot(t *testing.T) {
 			errorString:    "cannot create bot with an empty id",
 		},
 		{
+			name:           "name is empty",
+			input:          BotOptions{Id: "1"},
+			expectedOutput: nil,
+			errorExpected:  true,
+			errorString:    "cannot create bot with an empty name",
+		},
+		{
 			name: "Bot gets created successfully",
 			input: BotOptions{
-				Id: "123",
+				Id:   "123",
+				Name: "some name",
 			},
 			expectedOutput: &Bot{
-				id: "123",
+				id:   "123",
+				name: "some name",
 			},
 			errorExpected: false,
 			errorString:   "",
@@ -42,6 +51,27 @@ func Test_NewBot(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 			}
+			assert.Equal(t, tt.expectedOutput, result)
+		})
+	}
+}
+
+func Test_RandomBotNames(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          int64
+		expectedOutput []string
+	}{
+		{
+			name:           "random names are generated",
+			input:          10,
+			expectedOutput: []string{"The Hivey-five", "Gart", "RoboCupp", "T-3PO", "Avis"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := RandomBotNames(tt.input)
 			assert.Equal(t, tt.expectedOutput, result)
 		})
 	}
