355880c06b17cef1deabf048dc641b6d21786ba8
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-02-16 19:32:12 +0530

    Added player model

---
+++ internal/model/player.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"github.com/pkg/errors"
+	"github.com/vipulvpatil/airetreat-go/internal/utilities"
+)
+
+type Player struct {
+	id string
+}
+
+type PlayerOptions struct {
+	Id string
+}
+
+func NewPlayer(opts PlayerOptions) (*Player, error) {
+	if utilities.IsBlank(opts.Id) {
+		return nil, errors.Errorf("cannot create player with an empty id")
+	}
+	return &Player{
+		id: opts.Id,
+	}, nil
+}
---
+++ internal/model/player_test.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewPlayer(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          PlayerOptions
+		expectedOutput *Player
+		errorExpected  bool
+		errorString    string
+	}{
+		{
+			name:           "id is empty",
+			input:          PlayerOptions{},
+			expectedOutput: nil,
+			errorExpected:  true,
+			errorString:    "cannot create player with an empty id",
+		},
+		{
+			name: "Player gets created successfully",
+			input: PlayerOptions{
+				Id: "123",
+			},
+			expectedOutput: &Player{
+				id: "123",
+			},
+			errorExpected: false,
+			errorString:   "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := NewPlayer(tt.input)
+			if tt.errorExpected {
+				assert.EqualError(t, err, tt.errorString)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.expectedOutput, result)
+		})
+	}
+}
