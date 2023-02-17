de7374857df46b0d625296b674be5c63620d609b
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-02-17 18:10:06 +0530

    Added an helper interface to storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -36,3 +36,11 @@ func NewDbStorage(opts StorageOptions) (*Storage, error) {
 		IdGenerator: opts.IdGenerator,
 	}, nil
 }
+
+// A lot of queries/updates need to be part of a transaction but not all.
+// So we have the below interface that will allow the caller to either pass in *sql.DB or *sql.Tx depending on it's needs and our code will handle it without any issues.
+type customDbHandler interface {
+	Exec(query string, args ...any) (sql.Result, error)
+	Query(query string, args ...any) (*sql.Rows, error)
+	QueryRow(query string, args ...any) *sql.Row
+}
