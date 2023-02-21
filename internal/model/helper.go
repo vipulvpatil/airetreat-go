b3f85d1b8f8b22ec8a98194df1f2f77d2e0736a1
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-02-21 13:07:50 +0530

    Add more tesing helpers

--- internal/model/helper.go
+++ internal/model/helper.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// This function helps in testing Game Objects
+// These function helps in testing Game Objects
 func AssertEqualGame(t *testing.T, expected, actual *Game) {
 	assert.Equal(t, expected.id, actual.id, "game id is not equal")
 	assert.Equal(t, expected.state, actual.state, "game state is not equal")
@@ -20,9 +20,19 @@ func AssertEqualGame(t *testing.T, expected, actual *Game) {
 
 	// Since we cannot mock postgres time operations. We just check that the updated times are near expected times.
 	delta := 5 * time.Second
-	assert.WithinRange(t, actual.createdAt, expected.createdAt.Add(-1*delta), expected.createdAt.Add(delta), "game createdAt is not within range")
-	assert.WithinRange(t, actual.updatedAt, expected.updatedAt.Add(-1*delta), expected.updatedAt.Add(delta), "game updatedAt is not recent enough")
+	AssertTimeAlmostEqual(t, actual.createdAt, expected.createdAt, delta, "game createdAt is not within range")
+	AssertTimeAlmostEqual(t, actual.updatedAt, expected.updatedAt, delta, "game updatedAt is not within range")
 	if expected.stateHandledAt != nil {
-		assert.WithinRange(t, *actual.stateHandledAt, expected.stateHandledAt.Add(-1*delta), expected.stateHandledAt.Add(delta), "game stateHandledAt is not recent enough")
+		AssertTimeAlmostEqual(t, *actual.stateHandledAt, *expected.stateHandledAt, delta, "game stateHandledAt is not within range")
 	}
+}
+
+func AssertTimeAlmostEqual(t *testing.T, actual, expected time.Time, delta time.Duration, msgAndArgs ...interface{}) bool {
+	return assert.WithinRange(
+		t,
+		actual,
+		expected.Add(-1*delta),
+		expected.Add(delta),
+		msgAndArgs,
+	)
 }
