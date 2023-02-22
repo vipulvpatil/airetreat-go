1418393094232ee8f0f36f7b2550435c8aba88e1
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-02-22 12:55:28 +0530

    Added ability to create player in database

---
+++ internal/storage/create_player.go
@@ -0,0 +1,40 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/vipulvpatil/airetreat-go/internal/model"
+	"github.com/vipulvpatil/airetreat-go/internal/utilities"
+)
+
+func (s *Storage) CreatePlayer() error {
+	id := s.IdGenerator.Generate()
+
+	playerOpts := model.PlayerOptions{
+		Id: id,
+	}
+
+	_, err := model.NewPlayer(playerOpts)
+	if err != nil {
+		return utilities.WrapBadError(err, "failed to create player")
+	}
+
+	result, err := s.db.Exec(
+		`INSERT INTO public."players" ("id") VALUES ($1)`,
+		playerOpts.Id,
+	)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return utilities.WrapBadError(err, "dbError while inserting player and changing db")
+	}
+
+	if rowsAffected != 1 {
+		return utilities.NewBadError(fmt.Sprintf("Very few or too many rows were affected when inserting player in db. This is highly unexpected. rowsAffected: %d", rowsAffected))
+	}
+
+	return nil
+}
---
+++ internal/storage/create_player_test.go
@@ -0,0 +1,91 @@
+package storage
+
+import (
+	"database/sql"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vipulvpatil/airetreat-go/internal/utilities"
+)
+
+func Test_CreatePlayer(t *testing.T) {
+	tests := []struct {
+		name            string
+		setupSqlStmts   []string
+		cleanupSqlStmts []string
+		idGenerator     utilities.CuidGenerator
+		dbUpdateCheck   func(*sql.DB) bool
+		errorExpected   bool
+		errorString     string
+	}{
+		{
+			name:          "creates player successfully",
+			setupSqlStmts: []string{},
+			cleanupSqlStmts: []string{
+				`DELETE FROM public."players" WHERE id = 'player_id1'`,
+			},
+			idGenerator: &utilities.IdGeneratorMockConstant{Id: "player_id1"},
+			dbUpdateCheck: func(db *sql.DB) bool {
+				var (
+					id string
+				)
+				err := db.QueryRow(
+					`SELECT "id" FROM public."players" WHERE "id" = 'player_id1'`,
+				).Scan(&id)
+				assert.NoError(t, err)
+				assert.Equal(t, "player_id1", id)
+				return true
+			},
+			errorExpected: false,
+			errorString:   "",
+		},
+		{
+			name: "errors and does not update anything, if Player ID already exists in DB",
+			setupSqlStmts: []string{
+				`INSERT INTO public."players" ("id") VALUES ('id1')`,
+			},
+			cleanupSqlStmts: []string{
+				`DELETE FROM public."games" WHERE id = 'id1'`,
+			},
+			idGenerator: &utilities.IdGeneratorMockConstant{Id: "id1"},
+			dbUpdateCheck: func(db *sql.DB) bool {
+				var id string
+				err := db.QueryRow(
+					`SELECT id FROM public."players" WHERE "id" = 'id1'`,
+				).Scan(&id)
+				assert.NoError(t, err)
+				assert.Equal(t, "id1", id)
+				return true
+			},
+			errorExpected: true,
+			errorString:   "pq: duplicate key value violates unique constraint \"players_pkey\"",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := NewDbStorage(
+				StorageOptions{
+					Db:          testDb,
+					IdGenerator: tt.idGenerator,
+				},
+			)
+
+			runSqlOnDb(t, s.db, tt.setupSqlStmts)
+			defer runSqlOnDb(t, s.db, tt.cleanupSqlStmts)
+
+			rand.Seed(0)
+			err := s.CreatePlayer()
+			if !tt.errorExpected {
+				assert.NoError(t, err)
+			} else {
+				assert.NotEmpty(t, tt.errorString)
+				assert.EqualError(t, err, tt.errorString)
+			}
+			if tt.dbUpdateCheck != nil {
+				assert.True(t, tt.dbUpdateCheck(s.db))
+			}
+		})
+	}
+}
