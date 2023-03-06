7c799d327fc9475c492db4b00a23d5900554a31d
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-03-06 21:20:35 +0530

    ability to get old games

--- internal/storage/get_games.go
+++ internal/storage/get_games.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"github.com/vipulvpatil/airetreat-go/internal/utilities"
 )
@@ -16,6 +18,45 @@ func (s *Storage) GetGames(playerId string) ([]string, error) {
 		WHERE player_id = $1
 		ORDER BY created_at DESC, id DESC
 		`, playerId,
+	)
+	if err != nil {
+		return nil, utilities.WrapBadError(err, "failed to select games")
+	}
+	defer rows.Close()
+
+	gameIds := []string{}
+
+	for rows.Next() {
+		var gameId string
+		err := rows.Scan(
+			&gameId,
+		)
+
+		if err != nil {
+			return nil, utilities.WrapBadError(err, "failed while scanning rows")
+		}
+
+		gameIds = append(gameIds, gameId)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, utilities.WrapBadError(err, "failed to correctly go through bot rows")
+	}
+	return gameIds, nil
+}
+
+func (s *Storage) GetOldGames(gameExpiryDuration time.Duration) ([]string, error) {
+	if gameExpiryDuration > -5*time.Minute {
+		return nil, errors.New("invalid game expiry duration. Max acceptable time is -5 minutes.")
+	}
+
+	rows, err := s.db.Query(
+		`SELECT id
+		FROM public."games"
+		WHERE created_at < $1
+		ORDER BY created_at DESC, id DESC
+		`, time.Now().Add(gameExpiryDuration),
 	)
 	if err != nil {
 		return nil, utilities.WrapBadError(err, "failed to select games")
--- internal/storage/get_games_test.go
+++ internal/storage/get_games_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -106,6 +107,110 @@ func Test_Game_GetGames(t *testing.T) {
 
 			rand.Seed(0)
 			games, err := s.GetGames(tt.input)
+			if !tt.errorExpected {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.output, games)
+			} else {
+				assert.NotEmpty(t, tt.errorString)
+				assert.EqualError(t, err, tt.errorString)
+			}
+		})
+	}
+}
+
+func Test_Game_GetOldGames(t *testing.T) {
+	tests := []struct {
+		name            string
+		input           time.Duration
+		output          []string
+		setupSqlStmts   []TestSqlStmts
+		cleanupSqlStmts []TestSqlStmts
+		errorExpected   bool
+		errorString     string
+	}{
+		{
+			name:            "errors when timeDuration is invalid",
+			input:           1 * time.Second,
+			output:          []string{},
+			setupSqlStmts:   nil,
+			cleanupSqlStmts: nil,
+			errorExpected:   true,
+			errorString:     "invalid game expiry duration. Max acceptable time is -5 minutes.",
+		},
+		{
+			name:   "returns a list of gameIds that are old",
+			input:  -1 * time.Hour,
+			output: []string{"game_id5", "game_id2", "game_id1"},
+			setupSqlStmts: []TestSqlStmts{
+				{
+					Query: `INSERT INTO public."games" ("id", "state", "current_turn_index", "turn_order", "state_handled", "created_at")
+					VALUES ('game_id1', 'STARTED', 0, Array['b','p1','b','p2'], false, $1)`,
+					Args: []any{time.Now().Add(-2 * time.Hour)},
+				},
+				{
+					Query: `INSERT INTO public."bots" ("id", "name", "type", "game_id")
+					VALUES ('bot_id1', 'bot1', 'AI', 'game_id1')`,
+				},
+				{
+					Query: `INSERT INTO public."games" ("id", "state", "current_turn_index", "turn_order", "state_handled", "created_at")
+					VALUES ('game_id2', 'STARTED', 0, Array['b','p1','b','p2'], false, $1)`,
+					Args: []any{time.Now().Add(-2 * time.Hour)},
+				},
+				{
+					Query: `INSERT INTO public."bots" ("id", "name", "type", "game_id")
+					VALUES ('bot_id2', 'bot2', 'AI', 'game_id2')`,
+				},
+				{
+					Query: `INSERT INTO public."games" ("id", "state", "current_turn_index", "turn_order", "state_handled")
+					VALUES ('game_id3', 'STARTED', 0, Array['b','p1','b','p2'], false)`,
+				},
+				{
+					Query: `INSERT INTO public."bots" ("id", "name", "type", "game_id")
+					VALUES ('bot_id3', 'bot3', 'AI', 'game_id3')`,
+				},
+				{
+					Query: `INSERT INTO public."games" ("id", "state", "current_turn_index", "turn_order", "state_handled")
+					VALUES ('game_id4', 'STARTED', 0, Array['b','p1','b','p2'], false)`,
+				},
+				{
+					Query: `INSERT INTO public."bots" ("id", "name", "type", "game_id")
+					VALUES ('bot_id4', 'bot4', 'AI', 'game_id4')`,
+				},
+				{
+					Query: `INSERT INTO public."games" ("id", "state", "current_turn_index", "turn_order", "state_handled", "created_at")
+					VALUES ('game_id5', 'STARTED', 0, Array['b','p1','b','p2'], false, $1)`,
+					Args: []any{time.Now().Add(-2 * time.Hour)},
+				},
+				{
+					Query: `INSERT INTO public."bots" ("id", "name", "type", "game_id")
+					VALUES ('bot_id5', 'bot5', 'AI', 'game_id5')`,
+				},
+			},
+			cleanupSqlStmts: []TestSqlStmts{
+				{Query: `DELETE FROM public."games" WHERE id = 'game_id1'`},
+				{Query: `DELETE FROM public."games" WHERE id = 'game_id2'`},
+				{Query: `DELETE FROM public."games" WHERE id = 'game_id3'`},
+				{Query: `DELETE FROM public."games" WHERE id = 'game_id4'`},
+				{Query: `DELETE FROM public."games" WHERE id = 'game_id5'`},
+			},
+			errorExpected: false,
+			errorString:   "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := NewDbStorage(
+				StorageOptions{
+					Db: testDb,
+				},
+			)
+
+			runSqlOnDb(t, s.db, tt.setupSqlStmts)
+			defer runSqlOnDb(t, s.db, tt.cleanupSqlStmts)
+
+			rand.Seed(0)
+			games, err := s.GetOldGames(tt.input)
 			if !tt.errorExpected {
 				assert.NoError(t, err)
 				assert.Equal(t, tt.output, games)
