8dd35a41c049ff00855802d045e603a2d00ca222
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-03-09 18:00:29 +0530

    Added two nnew jobs to the worker pool

--- internal/workers/pool.go
+++ internal/workers/pool.go
@@ -7,6 +7,8 @@ import (
 )
 
 const START_GAME_ONCE_PLAYERS_HAVE_JOINED = "start_game_once_players_have_joined"
+const ASK_QUESTION_ON_BEHALF_OF_BOT = "ask_question_on_behalf_of_bot"
+const ANSWER_QUESTION_ON_BEHALF_OF_BOT = "answer_question_on_behalf_of_bot"
 const DELETE_EXPIRED_GAMES = "delete_expired_games"
 
 var workerStorage storage.StorageAccessor
@@ -21,6 +23,8 @@ func NewPool(deps PoolDependencies) *work.WorkerPool {
 	pool := work.NewWorkerPool(jobContext{}, 10, deps.Namespace, deps.RedisPool)
 
 	pool.Job(START_GAME_ONCE_PLAYERS_HAVE_JOINED, (*jobContext).startGameOncePlayersHaveJoined)
+	pool.Job(ASK_QUESTION_ON_BEHALF_OF_BOT, (*jobContext).askQuestionOnBehalfOfBot)
+	pool.Job(ANSWER_QUESTION_ON_BEHALF_OF_BOT, (*jobContext).answerQuestionOnBehalfOfBot)
 	pool.Job(DELETE_EXPIRED_GAMES, (*jobContext).deleteExpiredGames)
 
 	// TODO: Not sure if this is the best way to do this. But using Package variables for all dependencies required inside any of the jobs.
